efde5c7aed158f816f64ddefc1a5368ad472b618
PiyushMichael <piyush.michael93@gmail.com>
2019-08-01 01:29:15 +0530

    initialised and migrated

---
+++ app/Controllers/Http/CustomerController.js
@@ -0,0 +1,15 @@
+'use strict'
+
+class CustomerController {
+	async index() {}
+	
+	async store() {}
+	
+	async show() {}
+	
+	async update() {}
+	
+	async delete() {}
+}
+
+module.exports = CustomerController
